ae2224639036442c3408df37466c5bc97729b182
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-06-10 15:45:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,18 @@
-function addElementToBeginningOfArray(arr, e){
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function destructivelyAddElementToBeginningOfArray(arr, e){
   for(var i=arr.length; i>0; i--){
     arr[i] = arr[i-1];
   }
   arr[0] = e;
   return arr;
+}
+
+function addElementToBeginningOfArray(arr, e){
+  var newArray = []
+  for(var i=arr.length; i>0; i--){
+    newArray[i] = arr[i-1];
+  }
+  newArray[0] = e;
+  return newArray;
 }
